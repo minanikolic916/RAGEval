a567b4c29f777193693bf69a27593c378e037019
Mina <mina.nikolic@elfak.ni.ac.rs>
2025-02-05 17:25:57 +0100

    File change

--- inference.py
+++ inference.py
@@ -15,23 +15,6 @@ def get_quantization_config():
 
 def load_model_and_tokenizer(model_name:str):
     load_dotenv()
-    # if (model_name == "mistralai/Mistral-7B-Instruct-v0.3"):
-    #     model = AutoModelForCausalLM.from_pretrained(
-    #         model_name, 
-    #         quantization_config = get_quantization_config(), 
-    #         torch_dtype = torch.bfloat16,  
-    #         trust_remote_code = True,
-    #         token = os.getenv("HF_KEY")
-    #     )
-    # #za varijantu koja je fine-tjunovana
-    # else:
-    #     model = AutoPeftModelForCausalLM.from_pretrained(
-    #         model_name,
-    #         low_cpu_mem_usage=True,
-    #         torch_dtype=torch.float16,
-    #         load_in_4bit=True,
-    #         token = os.getenv("HF_KEY")
-    #     )
     model = AutoModelForCausalLM.from_pretrained(
             model_name, 
             quantization_config = get_quantization_config(), 
@@ -56,36 +39,12 @@ def load_pipeline(model, tokenizer):
     return pipe
 
 def get_model_response(pipe, context, question):
-    prompt = f"""<s>[INST] You are a helpful assistant.
-        You need to answer in Serbian language.
-        Use the following context to answer the question below comprehensively:
-        context = {context}
-        [/INST] </s>question = {question}
-        If you cannot answer with the context, just say "Nemam dovoljno informacija."
-        """
     prompt3 = f"""<s>[INST] Odgovori na postavljeno pitanje samo na osnovu prosledjenog konteksta. 
                             kontekst = {context}
                             [/INST] </s>pitanje = {question}
                             """
-    prompt_template = f"""
-    <|begin_of_text|><|start_header_id|>user<|end_header_id|>
-    You need to answer in Serbian language.
-    Use just the following context and no prior knowledge to answer the question below comprehensively:
-    context = {context}
-    Question: {question}<|eot_id|>
-    If you cannot answer using the context, dont answer. 
-    <|start_header_id|>assistant<|end_header_id|>
-    """
-    prompt_template2 = f"""<start_of_turn>user
-        Odgovori detaljno na pitanje samo na osnovu prosledjenog konteksta, a ne na osnovu prethodnog znanja. 
-        Odgovaraj samo na srpskom jeziku i to na cirilici.  
-        Kontekst = {context} 
-        Pitanje = {question}<end_of_turn>
-        <start_of_turn>model"""
-
     sequences = pipe(
         prompt3,
-        #prompt_template2,
         do_sample=True,
         max_new_tokens=300,
         temperature=0.6,
@@ -97,4 +56,4 @@ def get_model_response(pipe, context, question):
     ret_string = sequences[0]["generated_text"]
     answer_without_prompt = ret_string.replace(prompt3, " ")
     return answer_without_prompt
-    #return ret_string
+    
